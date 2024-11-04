fa9be1e45d09c008d08b07051cb2dbc3cbea39ea
CyberLelouch <167730646@qq.com>
2024-11-04 17:00:10 +0800

    03 - 使用EnableHystrix开启服务降级和熔断

--- borrow-service/src/main/java/com/test/BorrowApplication.java
+++ borrow-service/src/main/java/com/test/BorrowApplication.java
@@ -2,10 +2,12 @@ package com.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
+@EnableFeignClients//替代RestTemplate
+@EnableHystrix//开启服务降级
 public class BorrowApplication {
     public static void main(String[] args) {
         SpringApplication.run(BorrowApplication.class, args);
--- borrow-service/src/main/java/com/test/controller/BorrowController.java
+++ borrow-service/src/main/java/com/test/controller/BorrowController.java
@@ -1,5 +1,6 @@
 package com.test.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.test.entity.UserBorrowDetail;
 import com.test.service.BorrowService;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.Collections;
 
 @RestController
 public class BorrowController {
@@ -14,8 +16,15 @@ public class BorrowController {
     @Resource
     private BorrowService borrowService;
 
+    //指定服务降级方法
+    @HystrixCommand(fallbackMethod = "onError")
     @RequestMapping("/borrow/{uid}")
-    public UserBorrowDetail findUserBorrows(@PathVariable("uid") Integer uid) {
+    public UserBorrowDetail findUserBorrows(@PathVariable("uid") int uid) {
         return borrowService.getUserBorrowDetailById(uid);
+    }
+
+    //备选方案，返回空列表
+    UserBorrowDetail onError(int uid) {
+        return new UserBorrowDetail(null, Collections.emptyList());
     }
 }
